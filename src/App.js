8a38d6ab52880b03e357faffba44183de7559767
beerwithstraw <agarwalp@tcd.ie>
2020-11-07 02:43:38 +0000

    Basic container folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import {Cards, Chart, CountryPicker }from './components'
+import styles from './App.module.css'
+
 class App extends React.Component {
     render() {
         return (
-            <div>
+            <div className={styles.container}>
                 <h1>App</h1>
+                <Cards />
+                <Chart />
+                <CountryPicker />
             </div>
         )
     }
